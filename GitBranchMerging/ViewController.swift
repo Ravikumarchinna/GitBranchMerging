a040c9bf84c9bd8f04556fbba7212eefb77d1fad
Ravikumar <ravikumar66e@gmail.com>
2024-02-10 22:14:41 +0530

    1.Var Nuts Created

--- GitBranchMerging/ViewController.swift
+++ GitBranchMerging/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        var nums = [1,2,3,4,5,6]
+        print(nums)
+        
+        
     }
 
 
