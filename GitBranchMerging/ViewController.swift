13795caca8827635905442a37453b51c827418e7
Ravikumar <ravikumar66e@gmail.com>
2024-02-10 22:26:34 +0530

    1.Extra spaces removed

--- GitBranchMerging/ViewController.swift
+++ GitBranchMerging/ViewController.swift
@@ -11,13 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
+
         var nums = [1,2,3,4,5,6]
         print(nums)
-        
-        
     }
 
 
